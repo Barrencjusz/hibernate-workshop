e378d76cb2f1606685b6acbceb90fd9d642aea70
piteron <piotr.larysz.89@gmail.com>
2015-11-02 20:48:59 +0100

    Forgotten file change

--- hibernate-workshop/src/main/java/org/perform/hibernate/model/Game.java
+++ hibernate-workshop/src/main/java/org/perform/hibernate/model/Game.java
@@ -2,6 +2,7 @@ package org.perform.hibernate.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Game {
@@ -9,6 +10,8 @@ public class Game {
 	@Id
 	private Long id;
 
+	@ManyToOne
+	
 	public Long getId() {
 		return id;
 	}
